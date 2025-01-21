516406e74483f5d2492e38550829e0902710240a
EsAm1973 <me354172@gmail.com>
2025-01-21 22:09:38 +0200

    adding animation to spalsh view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,28 @@
 import 'package:bookly_app/Core/utils/Assets/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  double _opacirty = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    _animatedOpacity();
+  }
+
+  void _animatedOpacity() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _opacirty = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +30,21 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Image.asset(
-          AssetsPath.logo,
-          width: 200,
-          height: 200,
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-        const Text(
-          'Bookly App',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+        AnimatedImage(),
       ],
+    );
+  }
+
+  AnimatedOpacity AnimatedImage() {
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 500),
+      opacity: _opacirty,
+      curve: Curves.easeInOut,
+      child: Image.asset(
+        AssetsPath.logo,
+        width: 200,
+        height: 200,
+      ),
     );
   }
 }
