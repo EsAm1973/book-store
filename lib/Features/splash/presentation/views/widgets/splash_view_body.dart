243480afe2164c3ede15540e3bbb88a8caa899bb
EsAm1973 <me354172@gmail.com>
2025-01-21 20:30:17 +0200

    add text in splash ui

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -8,11 +8,23 @@ class SplashViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Image.asset(
           AssetsPath.logo,
           width: 200,
           height: 200,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const Text(
+          'Bookly App',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ],
     );
