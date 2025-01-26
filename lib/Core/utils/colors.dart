42d6603a9fca47c48b7ea06eeb915348a27bcfcb
EsAm1973 <me354172@gmail.com>
2025-01-27 00:37:45 +0200

    Make the Title and Sub title to details view

---
+++ lib/Core/utils/colors.dart
@@ -0,0 +1,6 @@
+
+
+//*****************************************************//
+// This class is the colors of app use it on other apps
+// The colors depends on the theme of the app
+//*****************************************************//
--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar_bookdetails.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,26 @@ class BookDetailsViewBody extends StatelessWidget {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: width * .18),
               child: const CustomBookImage(),
+            ),
+            const SizedBox(
+              height: 43,
+            ),
+            const DefaultTextStyle(
+              style: Styles.textStyle30,
+              child: Text('The Jungle Book'),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: 0.8,
+              child: DefaultTextStyle(
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                ),
+                child: const Text('Rudyard Kipling'),
+              ),
             ),
           ],
         ),
