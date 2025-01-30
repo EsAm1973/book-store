97c4c7f0fcea98cb3f332c3efbab47a2fb5004ca
EsAm1973 <me354172@gmail.com>
2025-01-30 17:21:33 +0200

    Implement fetch newest books

--- lib/Features/home/data/repos/home_repo_implementaion.dart
+++ lib/Features/home/data/repos/home_repo_implementaion.dart
@@ -1,19 +1,31 @@
 import 'package:bookly_app/Core/errors/failure.dart';
+import 'package:bookly_app/Core/utils/api_service.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImplementaion implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplementaion(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?q=subject:Programming&Filtering=free-ebooks&Sorting=newest');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(
+          books); // Because you use Either so you must return right side;
+    } on Exception {
+      return left(ServerFailure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
-  }
-
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {}
 }
